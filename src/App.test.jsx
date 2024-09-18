4cc1bf66bb4b1a8b61330fa1c6890054d6374bdc
MitchLloyd <mitchell@ada.ac.uk>
2024-09-18 16:30:14 +0000

    remove tests and implementations

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,14 +2,7 @@ import { test } from "vitest";
 import { fireEvent, render, screen } from "@testing-library/react";
 import Quiz from "./components/Quiz";
 
-test("renders the initial UI", () => {
-  // pass props through to a component
-  // render the component we are testing
-  // check we get the initial phrase
-
-  // Arrange: define props
-  // Act: render the component we're testing
-  // Assert: check the functionality
+test.only("renders the starting phrase with hidden words", () => {
 
   const props = {
     words: [
@@ -24,54 +17,22 @@ test("renders the initial UI", () => {
 
   render(<Quiz {...props} />);
 
-  screen.getByText("I _ therefore _ am"); // will throw an error if it can't element with this text
+  screen.getByText("I _ therefore _ am");
 });
 
 test("can update the phrase with chosen word", () => {
-  const props = {
-    words: [
-      { word: "I", display: true },
-      { word: "think", display: false },
-      { word: "therefore", display: true },
-      { word: "I", display: false },
-      { word: "am", display: true },
-    ],
-    options: ["think", "therefore", "swim", "wonder"],
-  };
-  render(<Quiz {...props} />);
-  // access the button element
-  // click on it
-  // check the phrase has been updated
+ // setup props
 
-  fireEvent.click(screen.getByRole("button", { name: /think/ }));
-  screen.getByText("I think therefore _ am");
+ // render the Quiz component
 
+ // find the button with text "swim"
+ // click on this button
+
+ // check the text has been updated with the blank filled in
   fireEvent.click(screen.getByRole("button", { name: /swim/ }));
   screen.getByText("I think therefore swim am");
 });
 
 test("can check if user has inputted correct answer", () => {
-  const words = [
-    { word: "I", display: true },
-    { word: "think", display: false },
-    { word: "therefore", display: true },
-    { word: "I", display: false },
-    { word: "am", display: true },
-  ];
-  const props = {
-    words,
-    options: ["think", "therefore", "swim", "wonder", "I"],
-    answer: words.map(({ word }) => word).join(" "),
-  };
-  render(<Quiz {...props} />);
 
-  fireEvent.click(screen.getByRole("button", { name: /think/ }));
-  screen.getByText("I think therefore _ am");
-
-  fireEvent.click(screen.getByRole("button", { name: /I/ }));
-  screen.getByText("I think therefore I am");
-
-  fireEvent.click(screen.getByRole("button", { name: /check/i }));
-
-  screen.getByText("Well done!");
 });
